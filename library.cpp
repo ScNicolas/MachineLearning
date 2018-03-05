11bbd382f7d7c3b8c1a050cc9d4a2e15c3701f50
Nicolas <nicolas.sirac@orange.fr>
2018-03-05 18:03:44 +0100

    sign

--- library.cpp
+++ library.cpp
@@ -4,7 +4,22 @@
 
 class DllExport C
 {
-     void hello() {
+    void hello() {
         std::cout << "Hello, World!" << std::endl;
+    }
+
+    int sign(double w[], double x[]){
+        double sum=0;
+        for(int i=0;i<2;i++){
+            sum+=w[i]*x[i];
+        }
+        sum+=w[2];
+
+        if(sum>0)
+            return 1;
+        else if(sum <0)
+            return -1;
+        return 0;
+
     }
 };
