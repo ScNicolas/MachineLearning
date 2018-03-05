eeb46a7612c8dfead1af16fed4042cce0680c45c
Nicolas <nicolas.sirac@orange.fr>
2018-03-05 18:25:42 +0100

    generateWeight

--- library.cpp
+++ library.cpp
@@ -1,6 +1,8 @@
 #include "library.h"
 #define DllExport   __declspec( dllexport )
 #include <iostream>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>
 
 class DllExport C
 {
@@ -9,17 +11,30 @@ class DllExport C
     }
 
     int sign(double w[], double x[]){
+
         double sum=0;
-        for(int i=0;i<2;i++){
+        for(int i=1;i<=2;i++){
             sum+=w[i]*x[i];
         }
-        sum+=w[2];
+        sum+=w[0];
 
         if(sum>0)
             return 1;
         else if(sum <0)
             return -1;
         return 0;
-
     }
+
+    double generateWeight(int size, int minRange, int maxRange)[]{
+        double w[size];
+        for(int i=0;i<size;i++){
+            srand (time(NULL));
+            w[i]= rand() % maxRange + minRange;
+        }
+        return w;
+    }
+
+
+
+
 };
