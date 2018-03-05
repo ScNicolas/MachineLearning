c81a660f4822b60bd2de11e4dca3c0e75fa9f72e
Yousria <yousria.benchadi@gmail.com>
2018-03-05 16:10:59 +0100

    First commit

---
+++ cmake-build-debug/CMakeFiles/untitled.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled.dir/library.cpp.o"
+  "cyguntitled.pdb"
+  "cyguntitled.dll"
+  "libuntitled.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
