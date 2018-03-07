b1030d7bd2e8767801b2d20b80e23a6aa4ce6310
Yousria <yousria.benchadi@gmail.com>
2018-03-07 15:52:19 +0100

    regression

--- library.cpp
+++ library.cpp
@@ -105,4 +105,66 @@ extern "C"
 	__declspec(dllexport) int hello() {
 		return 10;
 	}
+
+__declspec(dllexport) void LinearRegression(double x[], double y[], int n, double *a, double *b) {
+    int i;
+    double xsum, ysum, xysum, xxsum;
+    double ai, bi;
+
+    xsum = 0.0;
+    ysum = 0.0;
+
+    xysum = 0.0;
+    xxsum = 0.0;
+
+    for (i = 0; i < n; i++) {
+        xsum = xsum + x[i];
+        ysum = ysum + y[i];
+        xysum = xysum + x[i] * y[i];
+        xxsum = xxsum + x[i] * x[i];
+    }
+    ai = (n * xysum - xsum * ysum) / (n * xxsum - xsum * xsum);
+    bi = (ysum - ai * xsum) / n;
+    *a = ai;
+    *b = bi;
+
 }
+}
+
+__declspec(dllexport) double *trainLinear(double& w[],int size, double points[], int nbrPoints) {
+
+    for (int i = 0; i < nbrPoints; i++) {
+        double point[size];
+        for (int j = 0; j < size; j++) {
+            point[j] = points[(i * size) + j];
+        }
+        double alpha = 0.1;
+        int signPoint = sign(w, point);
+        while (signPoint != point[0]) {
+            for (int i = 1; i < size; i++) {
+                w[i] = w[i] + (alpha * (point[0] - signPoint) * point[i]);
+            }
+            w[0] = w[0] + (alpha * (point[0] - signPoint));
+            signPoint = sign(w, point);
+        }
+    }
+    return w;
+}
+
+__declspec(dllexport) double* executeLinear(double w[],int size, double& points[], int nbr_points) {
+    for(int i=0;i<nbr_points;i++){
+        double point[size];
+
+        for (int j=0; j < size; j++) {
+            point[j] = points[(i * size) + j];
+        }
+        points[(i * size)]=sign(w,point);
+    }
+    return points;
+
+}
+
+
+
+
+
